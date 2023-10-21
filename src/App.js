fdba8ca2f40b4c65ef4cd17e9cb57fca79d5718c
Sontara444 <sontararajput@gmail.com>
2023-10-22 00:38:17 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
-import logo from '../Assets/logo.png'
-import cart_icon from 'Assets/cart_icon'
-
 function App() {
   return (
     <div >
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Navbar.css'
 
+// import search from '../Assets/search.png'
+// import profile from '../Assets/profile.png'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className='nav-menu'>
+            <li>SHOP</li>
+            <li>MEN</li>
+            <li>WOMEN</li>
+            <li>KIDS</li>
+            <li>MEN</li>
+            <li>WOMEN</li>
+            <li>KIDS</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            
+            {/* <img src={search} alt="" />
+            <img src={profile} alt="" /> */}
+            <img src={cart_icon} alt="" />
+            
+        </div>
+    </div>
   )
 }
 
